7abf05f4e29ad9be87015bae8cab03f12b3c4b3a
carrcarl <carrcarl@DEL5484.ppal.directory>
2017-07-14 15:09:46 +0200

    Revert "third commit"
    This reverts commit f28ea0833c4cdb63ea030e4d016777b9725d79aa.

--- TestGitEclipse/src/com/client/Demo.java
+++ TestGitEclipse/src/com/client/Demo.java
@@ -5,7 +5,6 @@ public class Demo {
 	public static void main(String[] args) {
 		System.out.println("first commit");
 		System.out.println("second commit");
-		System.out.println("3 commit");
 
 	}
 
