3a1b1b2b48b63050bbb50aa87347e4e874aa3d67
carrcarl <carrcarl@DEL5484.ppal.directory>
2017-07-14 15:55:20 +0200

    5th commit corrections

--- TestGitEclipse/src/com/client/Demo.java
+++ TestGitEclipse/src/com/client/Demo.java
@@ -8,6 +8,7 @@ public class Demo {
 		System.out.println("33333 commit");
 		System.out.println("doing changes");
 		System.out.println("after aplying hard reset to the last commit");
+		System.out.println("5 commit corrections!");
 
 	}
 
